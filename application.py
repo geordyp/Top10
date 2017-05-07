e7d1ccb671c2a116838f140adab4455ad0d66e5c
Reagan Wood <reaganwood1@gmail.com>
2017-05-06 23:00:32 -0500

    Add table styling with ink framework

--- application.py
+++ application.py
@@ -47,7 +47,7 @@ def showCategory(category_url):
         listItems = session.query(ListItem).filter_by(list_id=l.id).order_by(asc(ListItem.position)).all()
         allListsWithItems.append(listItems)
 
-    return render_template('category.html', listsWithItems=allListsWithItems)
+    return render_template('categorytable.html', listsWithItems=allListsWithItems)
 
 
 if __name__ == '__main__':
