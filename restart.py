dad5394a625e58ec79d5d4eb0ca147e27dfca74d
carrington10 <carrington10@ksu.edu>
2017-05-08 21:05:38 -0700

    this is for user authentification

--- restart.py
+++ restart.py
@@ -8,10 +8,8 @@ from sqlalchemy.orm import sessionmaker
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 
-from database_setup import Base, UserAccount, Category, ListItem, List
-
 from functools import wraps
-from database_setup import Base
+
 import string
 import random
 import json
@@ -19,13 +17,6 @@ import httplib2
 import requests
 import os
 app = Flask(__name__)
-
-APPLICATION_NAME = 'Top 10'
-
-engine = create_engine('sqlite:///top10.db')
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
 
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
@@ -63,26 +54,6 @@ def getsession():
         return login_session['user']
                  
 
-@app.route('/')
-@app.route('/top10')
-def showHome():
-    """ Show the home page """
-    data = session.query(Category).all()
-    return render_template('homescreen.html', categories=data)
-
-
-@app.route('/top10/<string:category_url>')
-def showCategory(category_url):
-    """ Display category page with all top ten lists """
-    # retrieve data
-    category = session.query(Category).filter_by(url=category_url).one()
-    allLists = session.query(List).filter_by(category_id=category.id).order_by(asc(List.date_created)).all()
-    allListsWithItems = []
-    for l in allLists:
-        listItems = session.query(ListItem).filter_by(list_id=l.id).order_by(asc(ListItem.position)).all()
-        allListsWithItems.append(listItems)
-
-    return render_template('categorytable.html', listsWithItems=allListsWithItems)
     return 'Not logged in!'
 
 @app.route('/dropsesssion')
@@ -90,20 +61,13 @@ def dropsession():
      login_session.pop('user',None)
      return 'Dropped!'
 
-@app.route('/register',methods=['GET','POST'])
+@app.route('/register')
 def register():
-     if request.method == 'GET':  
-   	return render_template('register.html')
-     if request.method == 'POST':
-        email = request.form['Email']
-        name = request.form['username']
-        password = request.form['pwd']
-       
-	               
+   return render_template('register.html')
+                 
 
 if __name__ == '__main__':
      app.run(debug=True)
-
 
 
 if __name__ == '__main__':
