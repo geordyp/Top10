d56a4a218bc98092e05f731c8f0c3e73a61a33bc
geordyp <geowilliams6@gmail.com>
2017-05-11 00:40:45 -0500

    Edit and delete list items

--- application.py
+++ application.py
@@ -81,6 +81,7 @@ def newTopTenList(category_url):
 
 @app.route('/top10/list/<string:list_id>/edit')
 def editTopTenList(list_id):
+    """ Edit a top ten list """
     # TODO 404 list not found
     topTenList = session.query(List).filter_by(id=list_id).one()
     listItems = session.query(ListItem).filter_by(list_id=list_id).order_by(asc(ListItem.position)).all()
@@ -98,6 +99,7 @@ def editTopTenList(list_id):
 
 @app.route('/top10/list/<string:list_id>/item/new', methods=['GET', 'POST'])
 def newListItem(list_id):
+    """ Create a new list item """
     # TODO 404 list not found
     topTenList = session.query(List).filter_by(id=list_id).one()
 
@@ -115,7 +117,9 @@ def newListItem(list_id):
     if request.method == 'GET':
         return render_template('listItem_form.html',
                                list=topTenList,
+                               id="",
                                position=newItemPosition,
+                               editing=False,
                                title="",
                                description="",
                                error="")
@@ -132,10 +136,102 @@ def newListItem(list_id):
             error = "Please include a title."
             return render_template('listItem_form.html',
                                    list=topTenList,
+                                   id="",
                                    position=newItemPosition,
+                                   editing=False,
                                    title=request.form['title'],
                                    description=request.form['description'],
                                    error=error)
+
+
+@app.route('/top10/item/<string:listItem_id>/edit', methods=['GET', 'POST'])
+def editListItem(listItem_id):
+    """ Edit a list item """
+    # TODO 404 item not found
+    editedListItem = session.query(ListItem).filter_by(id=listItem_id).one()
+
+    if request.method == 'GET':
+        return render_template('listItem_form.html',
+                               list=editedListItem.list,
+                               id=listItem_id,
+                               position=editedListItem.position,
+                               editing=True,
+                               title=editedListItem.title,
+                               description=editedListItem.description,
+                               error="")
+    else:
+        if request.form['title']:
+            editedListItem.title = request.form['title']
+        else:
+            error = "Please include a title."
+            return render_template('listItem_form.html',
+                                   list=editedListItem.list,
+                                   id=listItem_id,
+                                   position=request.form['position'],
+                                   editing=True,
+                                   title=request.form['title'],
+                                   description=request.form['description'],
+                                   error=error)
+
+        editedListItem.description = request.form['description']
+
+        if request.form['position']:
+            newItemPosition = int(request.form['position'])
+            if newItemPosition != editedListItem.position:
+                if newItemPosition > 0 and newItemPosition < 11:
+                    listItems = session.query(ListItem).filter_by(list_id=editedListItem.list_id).order_by(asc(ListItem.position)).all()
+                    if len(listItems) < newItemPosition:
+                        swapListItem = session.query(ListItem).filter_by(list_id=editedListItem.list_id, position=len(listItems)).order_by(asc(ListItem.position)).one()
+                        swapListItem.position = editedListItem.position
+                        editedListItem.position = newItemPosition
+                        session.add(swapListItem)
+                    else:
+                        swapListItem = session.query(ListItem).filter_by(list_id=editedListItem.list_id, position=newItemPosition).order_by(asc(ListItem.position)).one()
+                        swapListItem.position = editedListItem.position
+                        editedListItem.position = newItemPosition
+                        session.add(swapListItem)
+                else:
+                    error = "Position needs to be between 1 and 10."
+                    return render_template('listItem_form.html',
+                                           list=editedListItem.list,
+                                           id=listItem_id,
+                                           position=request.form['position'],
+                                           editing=True,
+                                           title=request.form['title'],
+                                           description=request.form['description'],
+                                           error=error)
+        else:
+            error = "Please include a position."
+            return render_template('listItem_form.html',
+                                   list=editedListItem.list,
+                                   id=listItem_id,
+                                   position=request.form['position'],
+                                   editing=True,
+                                   title=request.form['title'],
+                                   description=request.form['description'],
+                                   error=error)
+
+        session.add(editedListItem)
+        session.commit()
+        return redirect(url_for('editTopTenList', list_id=editedListItem.list_id))
+
+
+@app.route('/top10/item/<string:listItem_id>/delete')
+def deleteListItem(listItem_id):
+    """ Delete a list item """
+    # TODO 404 not found
+    deletedListItem = session.query(ListItem).filter_by(id=listItem_id).one()
+
+    # shift the remaining list items
+    listItems = session.query(ListItem).filter_by(list_id=deletedListItem.list_id).order_by(asc(ListItem.position)).all()
+    if deletedListItem.position != len(listItems):
+        for i in range(deletedListItem.position - 1, len(listItems) - 1):
+            listItems[i + 1].position = listItems[i + 1].position - 1
+
+    session.delete(deletedListItem)
+    session.commit()
+    return redirect(url_for('editTopTenList', list_id=deletedListItem.list_id))
+
 
 
 if __name__ == '__main__':
